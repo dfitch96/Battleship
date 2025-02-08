95aec9f568384426dd9089105b8ed536cf35b661
Dylan Fitch <kkarl30@gmail.com>
2025-02-08 14:26:56 -0500

    started Ship class and some tests

---
+++ src/modules/ship.js
@@ -0,0 +1,34 @@
+
+
+class Ship{
+
+    constructor(length){
+        if(typeof length !== "number"){
+            throw new TypeError('ship constructor takes length of type number')
+        }
+
+        this.length = length;
+        this.hits = 0;
+        this.sunk = false;
+    }
+
+    getHits(){
+        return this.hits;
+    }
+
+    hit(){
+        if(!this.isSunk()){
+            this.hits++;
+        }
+    }
+
+    isSunk(){
+        if(this.hits === this.length){
+            this.sunk = true;
+        }
+        return this.sunk;
+    }
+}
+
+
+module.exports = Ship;
